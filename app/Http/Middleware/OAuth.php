6678c678270a46282357be73eb15c6364bfd6b54
radision <radision@126.com>
2016-05-17 18:40:50 +0800

    send authorize request

--- app/Http/Middleware/OAuth.php
+++ app/Http/Middleware/OAuth.php
@@ -16,17 +16,23 @@ class OAuth
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        // 包含 HTTP_AUTHORIZE ，做为access token使用，
+        // cookie 中有 access token 时，使用 access token 访问 API，
         // 否则跳转到open授权页面
         $access_token = $request->cookie('access_token');
         if (!$access_token)
         {
             $oauth_server = config('oauth.server');
             $authorize = config('oauth.authorize');
-            echo "oauth_server = $oauth_server\n";
-            echo "authorize = $authorize\n";
-            exit();
-            // return redirect();
+            $client_id = config('oauth.client_id');
+            $params = array(
+                'response_type'     => 'code',
+                'client_id'         => $client_id,
+                'state'             => md5($client_id.$authorize.$oauth_server.time()),
+                'redirect_uri'      => 'http://gift.radision.biz',
+            );
+            $params_str = http_build_query($params);
+            $url = "{$oauth_server}{$authorize}?{$params_str}";
+            return redirect($url);
         }
     }
 
