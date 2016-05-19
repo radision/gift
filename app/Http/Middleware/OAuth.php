3cb85380da0e37db554fdac73057be7c29b112b9
radision <radision@126.com>
2016-05-19 09:32:31 +0800

    fetch access token

--- app/Http/Middleware/OAuth.php
+++ app/Http/Middleware/OAuth.php
@@ -21,18 +21,47 @@ class OAuth
         $access_token = $request->cookie('access_token');
         if (!$access_token)
         {
+            $redirect_uri = 'http://gift.radision.biz';
+            $code = $request->input('code');
             $oauth_server = config('oauth.server');
-            $authorize = config('oauth.authorize');
-            $client_id = config('oauth.client_id');
-            $params = array(
-                'response_type'     => 'code',
-                'client_id'         => $client_id,
-                'state'             => md5($client_id.$authorize.$oauth_server.time()),
-                'redirect_uri'      => 'http://gift.radision.biz',
+            if (!$code)
+            {
+                $authorize = config('oauth.authorize');
+                $client_id = config('oauth.client_id');
+                $params = array(
+                    'response_type'     => 'code',
+                    'client_id'         => $client_id,
+                    'state'             => md5($client_id.$authorize.$oauth_server.time()),
+                    'redirect_uri'      => $redirect_uri,
+                );
+                $params_str = http_build_query($params);
+                $url = "{$oauth_server}{$authorize}?{$params_str}";
+                return redirect($url);
+            }
+            $post_data = array(
+                'grant_type'        => 'authorization_code',
+                'client_id'         => config('oauth.client_id'),
+                'client_secret'     => config('oauth.client_secret'),
+                'redirect_uri'      => $redirect_uri,
+                'code'              => $code,
             );
-            $params_str = http_build_query($params);
-            $url = "{$oauth_server}{$authorize}?{$params_str}";
-            return redirect($url);
+            $access_token = config('oauth.access_token');
+            $fetch_access_token_url = $oauth_server.$access_token;
+            $handle = curl_init($fetch_access_token_url);
+            curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($handle, CURLOPT_POST, true);
+            curl_setopt($handle, CURLOPT_POSTFIELDS, $post_data);
+            curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
+            $resp = curl_exec($handle);
+            echo $resp;exit();
+            if (curl_errno($handle))
+            {
+                echo curl_error($handle);
+                exit();
+            }
+            curl_close();
+            $result = json_decode($resp, true);
+            print_r($result);exit();
         }
     }
 
